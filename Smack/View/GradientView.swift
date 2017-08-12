2b9e70f394fbee4a2db373c66fcb1de44a9692e5
Marcus Ng <marcuslistanng@gmail.com>
2017-08-12 17:04:39 -0400

    UIView Gradient and ChannelVC UI

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,35 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Marcus Ng on 8/12/17.
+//  Copyright © 2017 Marcus Ng. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable // Renderable inside storyboard
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout() // Update layout view
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout() // Update layout view
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+}
