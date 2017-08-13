c86d3c81bb3da3e6237462218de16d938ce507e5
Marcus Ng <marcuslistanng@gmail.com>
2017-08-12 21:59:08 -0400

    Heroku and web request

---
+++ Smack/Services/AuthService.swift
@@ -0,0 +1,67 @@
+//
+//  AuthService.swift
+//  Smack
+//
+//  Created by Marcus Ng on 8/12/17.
+//  Copyright © 2017 Marcus Ng. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn: Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            if response.result.error == nil {
+                completion(true) // Success
+            } else {
+                completion(false) // Failure
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+    
+}
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+// Custom closure
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://slackclonesmack.herokuapp.com/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
